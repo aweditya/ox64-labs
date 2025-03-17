927fc1a1277ac271f2d9e2dda0eade953ee219c7
Aditya <aweditya@gmail.com>
2025-03-16 22:06:52 -0700

    plic is not plic-ing yet

--- tmp/plic.c
+++ tmp/plic.c
@@ -84,34 +84,53 @@ enum {
     MATCH_CMP2 = 3,
 };
 
-void put32_check(volatile uint32_t *addr, uint32_t v) {
-    uart_puts(UART0, "88\n");
-    put32(addr, v);
-    uart_puts(UART0, "90\n");
-    if (get32(addr) != v) {
-        uart_puts(UART0, "fuck");
-    }
-    uart_puts(UART0, "94\n");
+// Helper function to convert an integer to a hexadecimal string and print using putc
+static void itoa_hex(uint64_t num) {
+    static const char hex_digits[] = "0123456789ABCDEF";
+
+    // Print the '0x' prefix
+    uart_putc(UART0, '0');
+    uart_putc(UART0, 'x');
+
+    // Print each hexadecimal digit
+    for (int i = 60; i >= 0; i -= 4) {
+        uart_putc(UART0, hex_digits[(num >> i) & 0xF]);
+    }   
+    uart_putc(UART0, '\n');
 }
-void PUT32_check(volatile uint32_t addr, uint32_t v) {
-    uart_puts(UART0, "97\n");
-    PUT32(addr, v);
-    uart_puts(UART0, "99\n");
-    if (GET32(addr) != v) {
-        uart_puts(UART0, "fuck");
+
+void put32_check(volatile uint32_t *addr, uint32_t v) {
+    uart_puts(UART0, "Writing to address: ");
+    itoa_hex((uint64_t)addr);
+    put32(addr, v);
+    uart_puts(UART0, "Finished writing\n");
+    if (get32(addr) != v) {
+        uart_puts(UART0, "Write failed\n");
+    } else {
+        uart_puts(UART0, "Write succeeded\n");
     }
-    uart_puts(UART0, "103\n");
+}
+
+void PUT32_check(uint32_t addr, uint32_t v) {
+    uart_puts(UART0, "Writing to address: ");
+    itoa_hex((uint64_t)addr);
+    PUT32(addr, v);
+    uart_puts(UART0, "Finished writing\n");
+    if (GET32(addr) != v) {
+        uart_puts(UART0, "Write failed\n");
+    } else {
+        uart_puts(UART0, "Write succeeded\n");
+    }
 }
 
 // initialize timer1 with one match/comparator value
-void timer1_init(uint32_t match_val0) {
-
+void timer1_init(uint32_t match_val0, uint32_t match_val1, uint32_t match_val2) {
     // Disable timers and clear, page 492. write 1 to clear bits for both timer counters
     // write 0s to enable bits
     PUT32_check(timer_ctr_en_clr, (0b11 << 5));
 
     // clear interrupts
-    PUT32_check(timer1_intr_clr, 0b111);
+    PUT32(timer1_intr_clr, 0b111);
 
     // set mode to preload
     PUT32_check(timer_ctr_mode, 0);
@@ -119,18 +138,21 @@ void timer1_init(uint32_t match_val0) {
     // set preload value to 0
     PUT32_check(timer1_preload_val, 0);
 
-    // set preload mode to match on comparator 0
-    PUT32_check(timer1_preload_ctl, 1);
+    // set preload mode to match on comparator on the highest
+    // assumption match_val0 < match_val1 < match_val2
+    PUT32_check(timer1_preload_ctl, MATCH_CMP2);
 
-    // set comparator 0 match value to argument
+    // set comparator match values 
     PUT32_check(timer1_match_val0, match_val0);
+    PUT32_check(timer1_match_val1, match_val1);
+    PUT32_check(timer1_match_val2, match_val2);
 
     // set clock source, page 482
     // Set clock to f32k
     uint32_t x = GET32(timer_clk_src);
     // mask out bits 7:4
-    x &= (0b1111 << 4);
-    x |= (0b10000);
+    x &= ~(0b1111 << 4);
+    x |= SRC_32K << 4;
     PUT32_check(timer_clk_src, x);
 
     // setup the clock divider. For now write 0 - no freq div
@@ -138,7 +160,12 @@ void timer1_init(uint32_t match_val0) {
     // and so on...
     PUT32_check(timer_clk_div, 0);
 
-    // Enable timer3 (timer1) by writing 1 to bit 2
+    // enable timer1 interrupts for match val 0
+    PUT32_check(timer1_match_intr_en, 0b111);
+}
+
+void timer1_start(void) {
+    // Enable timer1 by writing 1 to bit 2
     PUT32_check(timer_ctr_en_clr, 0b100);
 }
 
@@ -156,21 +183,6 @@ void timer1_init(uint32_t match_val0) {
 
 // M-mode claim/complete register
 #define PLIC_H0_MCLAIM      0x0200004
-
-// Helper function to convert an integer to a hexadecimal string and print using putc
-void itoa_hex(uint64_t num) {
-    static const char hex_digits[] = "0123456789ABCDEF";
-
-    // Print the '0x' prefix
-    uart_putc(UART0, '0');
-    uart_putc(UART0, 'x');
-
-    // Print each hexadecimal digit
-    for (int i = 60; i >= 0; i -= 4) {
-        uart_putc(UART0, hex_digits[(num >> i) & 0xF]);
-    }   
-    uart_putc(UART0, '\n');
-}
 
 static inline uint64_t get_mtvec(void) {
     uint64_t result;
@@ -222,10 +234,13 @@ static inline void vector_base_set(void *vec) {
 
 // pg 89 of c906 doc
 static uint32_t intr_req_response(void) {
+    uint64_t mapbaddr = get_mapbaddr();
+
     // 1. read claim/complete reg
     // the read operation resets the ip bit of
     // the corresponding plic
-    uint32_t mclaim_reg = get32(PLIC_H0_MCLAIM);
+    uint32_t *plic_h0_mclaim = (uint32_t *)(PLIC_H0_MCLAIM + mapbaddr);
+    uint32_t mclaim_reg = get32(plic_h0_mclaim);
     itoa_hex(mclaim_reg);
 
     // 2. if mclaim_reg == 0, return
@@ -244,12 +259,18 @@ static uint32_t intr_req_response(void) {
 
 // pg 89 of c906
 static void intr_completion(uint32_t target) {
+    uint64_t mapbaddr = get_mapbaddr();
+
     // write id of intr to corresponding claim/complete reg
-    put32_check(PLIC_H0_MCLAIM, target);
+    uint32_t *plic_h0_mclaim = (uint32_t *)(PLIC_H0_MCLAIM + mapbaddr);
+    uint32_t mclaim_reg = get32(plic_h0_mclaim);
+    put32_check(plic_h0_mclaim, target);
 }
 
 __attribute__((aligned(4))) void handler(void) {
-    uart_puts(UART0, "inside handler\n");
+    uart_puts(UART0, "Inside handler\n");
+    uint32_t stat = GET32(timer1_match_status);
+    itoa_hex(stat);
 
     uint64_t mepc;
     asm volatile("csrr %0, mepc" : "=r"(mepc));
@@ -260,15 +281,20 @@ __attribute__((aligned(4))) void handler(void) {
     uint64_t mtval;
     asm volatile("csrr %0, mtval" : "=r"(mtval));
 
+    uart_puts(UART0, "MEPC: ");
     itoa_hex(mepc);
     uart_putc(UART0, '\n');
+    uart_puts(UART0, "MCAUSE: ");
     itoa_hex(mcause);
     uart_putc(UART0, '\n');
+    uart_puts(UART0, "MTVAL: ");
     itoa_hex(mtval);
     uart_putc(UART0, '\n');
 
     // REMOVE: move back above mepc line 247??
     uint32_t target = intr_req_response();
+    uart_puts(UART0, "target: ");
+    itoa_hex((uint64_t)target);
     if (target == 0)
         return;
 
@@ -280,7 +306,9 @@ void set_intr_prio(uint32_t irq, unsigned prio) {
         // disallowed priority
         return;
 
-    put32_check(PLIC_PRIO_BASE + irq, prio);
+    uint64_t mapbaddr = get_mapbaddr();
+    uint32_t *plic_prio_base = (uint32_t *)(PLIC_PRIO_BASE + mapbaddr);
+    put32_check(plic_prio_base + irq, prio);
 }
 
 void set_intr_th(unsigned th) {
@@ -290,65 +318,141 @@ void set_intr_th(unsigned th) {
 
     // intr only triggered if
     // prio of intr > th
-    put32_check(PLIC_H0_MTH, th);
+    uint64_t mapbaddr = get_mapbaddr();
+    uint32_t *plic_h0_mth = (uint32_t *)(PLIC_H0_MTH + mapbaddr);
+    put32_check(plic_h0_mth, th);
 }
 
-
-void disable_interrupts(uint32_t irq) {
-    uint32_t ie_reg = get32(PLIC_H0_MIE_BASE + (irq / 32));
-    ie_reg &= ~(1 << (irq & 32));
-    put32_check(PLIC_H0_MIE_BASE + (irq / 32), ie_reg);
+uint32_t check_intr_pending(uint32_t irq) {
+    uint64_t mapbaddr = get_mapbaddr();
+    uint32_t *plic_ip_base = (uint32_t *)(PLIC_IP_BASE + mapbaddr);
+    uint32_t ip_reg = get32(plic_ip_base + (irq >> 5));
+    return (ip_reg >> (irq & 0b11111)) & 1;
 }
 
-void disable_all_interrupts(uint32_t *plic_h0_mie_base) {
+void disable_plic_interrupt(uint32_t irq) {
+    uint64_t mapbaddr = get_mapbaddr();
+    uint32_t *plic_h0_mie_base = (uint32_t *)(PLIC_H0_MIE_BASE + mapbaddr);
+
+    uint32_t ie_reg = get32(plic_h0_mie_base + (irq >> 5));
+    ie_reg &= ~(1 << (irq & 0b11111));
+    put32_check(plic_h0_mie_base + (irq >> 5), ie_reg);
+}
+
+void disable_all_plic_interrupts(void) {
+    uint64_t mapbaddr = get_mapbaddr();
+    uint32_t *plic_h0_mie_base = (uint32_t *)(PLIC_H0_MIE_BASE + mapbaddr);
+
     // disable all interrupts
-    for (int i=0; i<32; i++) {
+    // load access fault beyond 0x0002008 
+    for (int i=0; i<3; i++) {
         put32_check(plic_h0_mie_base + i, 0);
     }
 }
 
-void enable_interrupts(uint32_t irq) {
-    uint32_t ie_reg = get32(PLIC_H0_MIE_BASE + (irq / 32));
+void disable_ext_interrupts(void) {
+    asm volatile("li t0, 0x800");
+    asm volatile("csrrc t1, mie, t0");
+}
+
+void enable_ext_interrupts(void) {
+    asm volatile("li t0, 0x800");
+    asm volatile("csrrs t1, mie, t0");
+}
+
+void disable_interrupts(void) {
+    asm volatile("csrci mstatus, 0x8");
+}
+
+void enable_interrupts(void) {
+    asm volatile("csrsi mstatus, 0x8");
+}
+
+void enable_plic_interrupt(uint32_t irq) {
+    uint64_t mapbaddr = get_mapbaddr();
+    uint32_t *plic_h0_mie_base = (uint32_t *)(PLIC_H0_MIE_BASE + mapbaddr);
+
+    uint32_t ie_reg = get32(plic_h0_mie_base + (irq / 32));
     ie_reg |= 1 << (irq % 32);
-    put32_check(PLIC_H0_MIE_BASE + (irq / 32), ie_reg);
+    put32_check(plic_h0_mie_base + (irq / 32), ie_reg);
 }
 
 void kmain(void) {
   uart_init(UART0, 115200);
 
   uint64_t mapbaddr = get_mapbaddr();
-  uart_puts(UART0, "Printing PLIC and other in-core modules base addr: ");
-  itoa_hex(mapbaddr);
 
-  uint32_t *plic_prio_base = (uint32_t *)(PLIC_PRIO_BASE + mapbaddr);
-  uint32_t *plic_ip_base = (uint32_t *)(PLIC_IP_BASE + mapbaddr);
-  uint32_t *plic_h0_mie_base = (uint32_t *)(PLIC_H0_MIE_BASE + mapbaddr);
-  uint32_t *plic_h0_mth = (uint32_t *)(PLIC_H0_MTH + mapbaddr);
+  // disable all interrupts (plic + clint)
+  // this is a global disable
+  // even the timer and sw interrupts get
+  // disabled
+  disable_interrupts();
+
+  // disable external interrupts
+  disable_ext_interrupts();
+  
+  // disable plic interrupts
+  disable_all_plic_interrupts();
+
+  // clear outstanding interrupts
   uint32_t *plic_h0_mclaim = (uint32_t *)(PLIC_H0_MCLAIM + mapbaddr);
+  uint32_t mclaim_reg = get32(plic_h0_mclaim);
+  put32_check(plic_h0_mclaim, mclaim_reg);
 
+  // setup trap+exception handler
   vector_base_set((uint32_t *)handler);
 
-  // REMOVE
-  asm volatile("csrsi mstatus, 0x8");
-
-  disable_all_interrupts(plic_prio_base);
-
-  uart_puts(UART0, "309\n");
-  set_intr_prio(IRQ_NUM_BASE + 61, 1);
-  set_intr_prio(IRQ_NUM_BASE + 62, 1);
-  
+  // setup plic interrupt threshold
   set_intr_th(0);
-  
-  uart_puts(UART0, "314\n");
-  timer1_init(32000);
-  
-  enable_interrupts(IRQ_NUM_BASE + 61);
-  enable_interrupts(IRQ_NUM_BASE + 62);
 
+  uart_puts(UART0, "Starting setting interrupt priorities\n");
+
+  uart_puts(UART0, "Setting priority for interrupt #36");
+  set_intr_prio(IRQ_NUM_BASE + 36, 1);
+  uart_puts(UART0, "Setting priority for interrupt #37");
+  set_intr_prio(IRQ_NUM_BASE + 37, 1);
+  uart_puts(UART0, "Setting priority for interrupt #61");
+  set_intr_prio(IRQ_NUM_BASE + 61, 1);
+  uart_puts(UART0, "Setting priority for interrupt #62");
+  set_intr_prio(IRQ_NUM_BASE + 62, 1);
+
+  uart_puts(UART0, "Finished setting interrupt priorities\n");
+  
+  timer1_init(10000, 20000, 32000);
+  uint32_t ctr = GET32(timer1_ctr_val);
+  uart_puts(UART0, "Initial counter after timer init: ");
+  itoa_hex(ctr);
+  
+  // enable plic interrupts
+  uart_puts(UART0, "Enabling interrupt #36");
+  enable_plic_interrupt(IRQ_NUM_BASE + 36);
+  uart_puts(UART0, "Enabling interrupt #37");
+  enable_plic_interrupt(IRQ_NUM_BASE + 37);
+  uart_puts(UART0, "Enabling interrupt #61");
+  enable_plic_interrupt(IRQ_NUM_BASE + 61);
+  uart_puts(UART0, "Enabling interrupt #62");
+  enable_plic_interrupt(IRQ_NUM_BASE + 62);
+
+  // uart_puts(UART0, "Checking pending status of interrupt #");
+  // itoa_hex(i);
+  // uint32_t pending = check_intr_pending(IRQ_NUM_BASE + i);
+  // itoa_hex(pending);
+
+  // enable external interrupts
+  enable_ext_interrupts();
+
+  // start the timer
+  timer1_start();
+
+  // enable all interrupts (plic + clint)
+  // this is a global enable
   asm volatile("csrsi mstatus, 0x8");
 
   while(1) {
-      uart_puts(UART0, "hi\n");
-      delay_ms(1000);
+      // uint32_t ctr = GET32(timer1_ctr_val);
+      // itoa_hex(ctr);
+      uint32_t stat = GET32(timer1_match_status);
+      itoa_hex(stat);
+      // asm volatile("wfi");
   }
 }
