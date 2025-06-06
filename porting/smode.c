b2a125d48ca6bfc85f24cbf0c2998e55a4cab4bd
Aditya Sriram <aweditya@gmail.com>
2025-06-06 11:19:20 -0700

    finally managed to get into s-mode

--- porting/smode.c
+++ porting/smode.c
@@ -1,8 +1,11 @@
 #include "lib.h"
 #include "printk.h"
 
-void trap_vector(void) {
+void init_uart(void) {
     uart_init(UART0, 115200);
+}
+
+void trap_vector(void) {
     while(1) {
         uart_puts(UART0, "in trap!\r\n");
         delay_ms(1000);
@@ -58,13 +61,12 @@ static inline uint64_t csrr_satpval() {
     return val;
 }
 
-
-
-void print_in_trap() {
-    uart_puts(UART0, "in trap!!\n");
+void print_in_trap(void) {
+    uart_puts(UART0, "inside trap\n");
 }
 
 void dump_csrs(void) {
+    uart_puts(UART0, "dumping csrs...\n");
     uart_puts(UART0, "mstatus: "); uart_puthex64(csrr_mstatus()); uart_putc(UART0, '\n');
     uart_puts(UART0, "mepc:    "); uart_puthex64(csrr_mepc());    uart_putc(UART0, '\n');
     uart_puts(UART0, "mtvec:    "); uart_puthex64(csrr_mtvec());    uart_putc(UART0, '\n');
@@ -76,9 +78,11 @@ void dump_csrs(void) {
 }
 
 void kmain(void) {
-    uart_init(UART0, 115200);
+    init_uart();
     uart_puts(UART0, "hello, main!\r\n");
+
     while(1) {
+        uart_puts(UART0, "inside the while loop\r\n");
         dump_csrs();
         uart_puts(UART0, "hello, world!\r\n");
         delay_ms(1000);
